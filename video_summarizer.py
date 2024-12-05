8c4592ca0601fe59bab033bab8b90142cb96ae9e
Gustavo M <ghlsem@gmail.com>
2024-12-05 11:30:01 -0300

    changes

--- video_summarizer.py
+++ video_summarizer.py
@@ -27,10 +27,6 @@ class VideoSummarizer:
         self.summary_dir = summary_dir
         gc.collect()
         torch.cuda.empty_cache()
-        if self.model != None:
-            del self.model
-        if self.batched_model != None:
-            del self.batched_model
         gc.collect()
         torch.cuda.empty_cache()
                     # Ensure CUDA is available
