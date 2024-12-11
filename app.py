246c2f31b3968ee721876b88dd9a486f86201428
Gustavo M <ghlsem@gmail.com>
2024-12-11 05:43:09 -0300

    try fix

--- app.py
+++ app.py
@@ -43,10 +43,10 @@ def index():
             # Read transcription and summary
             with open(transcription_file, 'r') as f:
                 transcription = f.read()
-            summary_dir="/tmp/summaries"
-            summary_file = os.path.join(summary_dir, 'summary.txt')
-            with open(summary_file, 'r') as s_f:
-                summary = s_f.read()
+#            summary_dir="/tmp/summaries"
+ #           summary_file = os.path.join(summary_dir, 'summary.txt')
+#            with open(summary_file, 'r') as s_f:
+#                summary = s_f.read()
 
             return jsonify({
                 'summary': summary,
